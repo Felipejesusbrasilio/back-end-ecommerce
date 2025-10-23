e0207b66630cf8a268a30b6fbc8240ca9249c946
FELIPE DE JESUS BRASILIO DA COSTA <69780275+Felipejesusbrasilio@users.noreply.github.com>
2025-10-23 12:25:53 -0300

    Update index.js

--- back-end-e-commerce/index.js
+++ back-end-e-commerce/index.js
@@ -9,7 +9,7 @@ const rota = require('./router/rotas');
 
 // Configuração do CORS para permitir cookies entre domínios
 app.use(cors({
-  origin: 'http://localhost:3000',  // Permite o frontend
+  origin: 'https://front-e1df96fjb-felipejesusbrasilios-projects.vercel.app/',  // Permite o frontend
   credentials: true,  // Permite o envio de cookies de sessão
 }));
 
